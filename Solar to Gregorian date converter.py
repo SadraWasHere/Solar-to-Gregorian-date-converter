b64785eb8094b9aa8a87a80bf722b4c3d51675e3
SadraTashakori <Sadra.Tashakori.Ghoozhdi@gmail.com>
2026-02-24 20:35:03 +0330

    Turned "Solar to Gregorian date converter.py" into a python module, for ```SolToGreg()``` function.

---
+++ Solar to Gregorian date converter.py
@@ -0,0 +1,7 @@
+from datetime import datetime , date , time
+def shamsitomiladi(dt):
+    shamsi = datetime(2005, 12, 24)
+    miladi = datetime(1384, 10, 3)
+    delta = shamsi - miladi
+
+    return (dt - delta)
