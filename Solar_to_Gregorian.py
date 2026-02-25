c0b11ba8672152d3679ff33c72e98975ec9d0035
SadraTashakori <Sadra.Tashakori.Ghoozhdi@gmail.com>
2026-02-25 19:20:29 +0330

    Update: Also added ```GregToSol()```. In purpose of turning Gregorian date into Solar.

--- Solar_to_Gregorian.py
+++ Solar_to_Gregorian.py
@@ -5,3 +5,10 @@ def SolToGreg(dt):
     delta = shamsi - miladi
 
     return (dt - delta)
+
+def GregToSol(dt):
+    shamsi = datetime(2005, 12, 24)
+    miladi = datetime(1384, 10, 3)
+    delta = shamsi - miladi
+
+    return (dt + delta)
