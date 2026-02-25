8e1979a92df31fc0d8d3809d6fc0484944052e9a
SadraTashakori <Sadra.Tashakori.Ghoozhdi@gmail.com>
2026-02-25 19:29:18 +0330

    NAME OF THE CONVERTORS WERE OPPOSITE!!!

--- Solar_to_Gregorian.py
+++ Solar_to_Gregorian.py
@@ -1,12 +1,12 @@
 from datetime import datetime , date , time
-def SolToGreg(dt):
+def GregToSol(dt):
     shamsi = datetime(2005, 12, 24)
     miladi = datetime(1384, 10, 3)
     delta = shamsi - miladi
 
     return (dt - delta)
 
-def GregToSol(dt):
+def SolToGreg(dt):
     shamsi = datetime(2005, 12, 24)
     miladi = datetime(1384, 10, 3)
     delta = shamsi - miladi
